a9108b70510ead2187967053449f335d17e69a6b
pablodll <pablo.daurell@gmail.com>
2025-06-03 19:02:46 +0200

    Basic JPARepository for Country

---
+++ country-api-service/src/main/java/com/pablodll/country_api_service/repository/CountryRepository.java
@@ -0,0 +1,16 @@
+package com.pablodll.country_api_service.repository;
+
+import com.pablodll.country_api_service.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for {@link Country} entity.
+ * Basic CRUD methods to manage countries in the DB.
+ */
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    /// Basic CRUD methods implemented by default by JpaRepository
+
+}
