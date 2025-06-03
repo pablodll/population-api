b1b344cb20fb1621dc2b3df8fc47c244e17b9ef1
pablodll <pablo.daurell@gmail.com>
2025-06-03 17:07:53 +0200

    basic country-api-service spring structure

---
+++ country-api-service/src/main/java/com/pablodll/country_api_service/CountryApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pablodll.country_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryApiServiceApplication.class, args);
+	}
+
+}
---
+++ country-api-service/src/test/java/com/pablodll/country_api_service/CountryApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablodll.country_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
