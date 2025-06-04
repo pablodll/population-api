d03d713bfb5c3fbd9505c1361b4f58972096d186
pablodll <pablo.daurell@gmail.com>
2025-06-05 01:41:46 +0200

    ResponseDTO is now used for every response

--- country-api-service/src/main/java/com/pablodll/country_api_service/dto/CountryResponseDTO.java
+++ country-api-service/src/main/java/com/pablodll/country_api_service/dto/CountryResponseDTO.java
@@ -5,14 +5,24 @@ package com.pablodll.country_api_service.dto;
  */
 public class CountryResponseDTO {
 
+    private String code;
     private String name;
     private Long population;
 
     public CountryResponseDTO() {}
 
-    public CountryResponseDTO(String name, Long population) {
+    public CountryResponseDTO(String code, String name, Long population) {
+        this.code = code;
         this.name = name;
         this.population = population;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public String getName() {
