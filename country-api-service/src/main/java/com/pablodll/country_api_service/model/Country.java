31e7f3901160f0e886ca5edecedef43a2e850fd0
pablodll <pablo.daurell@gmail.com>
2025-06-05 01:47:03 +0200

    Removed lombok dependency and usage

--- country-api-service/src/main/java/com/pablodll/country_api_service/model/Country.java
+++ country-api-service/src/main/java/com/pablodll/country_api_service/model/Country.java
@@ -1,18 +1,12 @@
 package com.pablodll.country_api_service.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * Persistence class for countries database table
  */
 @Entity
 @Table(name = "countries")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Country {
 
     /**
@@ -28,6 +22,38 @@ public class Country {
 
     @Column(name = "population", nullable = false)
     private Long population;
+
+    public Country() {}
+
+    public Country(String code, String name, Long population) {
+        this.code = code;
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
 
     @Override
     public String toString() {
